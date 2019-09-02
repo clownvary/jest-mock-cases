d54852170e6df75aca957c32a2f4f19f11a3d718
clownvary <vary_007@163.com>
2019-09-02 23:18:04 +0800

    fix wrong ut

--- test/moduleClass/phone1.spec.js
+++ test/moduleClass/phone1.spec.js
@@ -1,8 +1,8 @@
 import Phone from 'phone';
 import SoundPlayer from 'soundPlayer';
 // 适合同一文件中mock多次不同的值
-// 注意当前这种方式只适合内部延迟执行class内的方法的调用
-// 不适合return new SoundPlayer().playSoundFile();这种直接调用
+// 注意当前这种方式只适合内部延迟执行class内的方法的调用, 有一点局限性
+// 不适合return new SoundPlayer().playSoundFile();这种直接调用请看phone3.spec.js
 
 jest.mock('soundPlayer');
 
@@ -16,11 +16,13 @@ describe('Module with class: need to replace implementation many times', () => {
     it('should return a mock value by manual mock',()=>{
         const phone = new Phone();
         expect(SoundPlayer).toHaveBeenCalled();
+        jest.fn().mockImplementation
         
         // 注意一定要在实例上mock
+        
         const mockSoundPlayerInstance = SoundPlayer.mock.instances[0];
         const spy = jest.spyOn(mockSoundPlayerInstance,'playSoundFile');
-        spy.mockReturnValue('mock value');
+        spy.mockReturnValue('mock value'); 
         expect(phone.playSomethingCool()).toEqual('mock value');
     })
     it('should return a mock value by manual mock2',()=>{
--- test/moduleClass/phoneX.spec.js
+++ test/moduleClass/phoneX.spec.js
@@ -8,8 +8,8 @@ describe('Module with class: need to replace implementation many times', () => {
         const phone = new Phone();
         // 直接mock default,而不论导出的是class还是普通的方法
         // 唯一区别是class的mock 需要实现constructor即
-        // xxx.default = jest.fn().mockImplementation((=>{
-        //     xxx// constructor
+        // xxx.default = jest.fn().mockImplementation(()=>{
+        //     xxx // constructor
         //     return {...}// instance methods
         // }))
         Sound.default = jest.fn().mockImplementation(() => ({
